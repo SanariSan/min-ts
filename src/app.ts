7aee8f69399b759fc5cfe848eff0ae8b098a6030
SanariSan <SanariSan@users.noreply.github.com>
2023-12-26 00:11:17 +0400

    feat: ts express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,41 @@
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import express from 'express';
+import { createServer } from 'http';
+import { ping } from './api';
+import { setupHandleErrors } from './error';
+import { syncHandleMW } from './handle';
+import { setupSettings } from './settings';
+import { NotFoundError } from './errors';
 
+function init() {
+  const app = express();
+  const server = createServer(app);
 
-async function init() {
+  server.on('error', (e: NodeJS.ErrnoException) => {
+    if (e.code === 'EADDRINUSE' || e.code === 'EADDRNOTAVAIL') {
+      setTimeout(() => {
+        server.close();
+
+        server.listen(Number(process.env.PORT ?? 80), process.env.HOST ?? '0.0.0.0');
+      }, 60_000);
+    }
+  });
+
+  setupSettings(app);
+
+  app.use('/ping', syncHandleMW(ping));
+  app.use(`*`, () => {
+    throw new NotFoundError({ message: 'Route not found' });
+  });
+
+  setupHandleErrors(app);
+
+  server
+    .listen(Number(process.env.PORT ?? 80), process.env.HOST ?? '0.0.0.0', () => {
+      console.log(`Server running on port : ${process.env.PORT ?? ':80'}`);
+    })
+    .on('error', (e) => {
+      console.log(e);
+    });
 }
 
-void init();
+init();
